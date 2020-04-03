312d3dea66983a9668a411bdb567883b7235d5d4
offcircuit <35690030+offcircuit@users.noreply.github.com>
2020-04-03 12:34:33 +0100

    Add files via upload

---
+++ tinyNextion.cpp
@@ -0,0 +1,192 @@
+#include "tinyNextion.h"
+
+tinyNextion::tinyNextion(uint8_t rx, uint8_t tx) {
+  _serial = new SoftwareSerial(rx, tx);
+}
+
+uint32_t tinyNextion::begin(uint32_t speed) {
+  uint32_t rate = baud();
+  if (speed && (speed != rate)) return setBaud(speed);
+  return rate;
+}
+
+void tinyNextion::attach(nextionEvent event, nextionOnEvent pointer) {
+  if (_callbacks) {
+    nextionCallback *item = _callbacks;
+    do if ((item->event.page == event.page) && (item->event.id == event.id) && (item->event.state == event.state)) {
+        item->pointer = pointer;
+        return;
+      } while (item->next && (item = item->next));
+    item->next = callback(event, pointer);
+  } else _callbacks = callback(event, pointer);
+}
+
+uint8_t tinyNextion::backlight(uint8_t value) {
+  return print("dim=" + String(value));
+}
+
+uint32_t tinyNextion::baud() {
+  const uint8_t map[8] = {48, 24, 16, 8, 4, 2, 1, 0};
+  uint8_t index = 0;
+
+  do _serial->begin(map[index] * 2400UL);
+  while (!connect() && (7 > ++index));
+
+  return map[index] * 2400UL;
+}
+
+tinyNextion::nextionCallback *tinyNextion::callback(nextionEvent event, nextionOnEvent pointer) {
+  nextionCallback *item = new nextionCallback;
+  item->next = NULL;
+  item->pointer = pointer;
+  item->event = event;
+  return item;
+}
+
+bool tinyNextion::connect() {
+  flush();
+  send("DRAKJHSUYDGBNCJHGJKSHBDN");
+  send("connect");
+  send(String(char(0xFF) + char(0xFF)) + "connect");
+  flush();
+  send("");
+  flush();
+  send("connect");
+
+  if (readln()) return _data.indexOf("comok") != -1;
+}
+
+void tinyNextion::flush() {
+  _signal = NEXTION_SERIAL_CYCLES;
+  do while (_serial->available()) {
+      _serial->read();
+      _signal = NEXTION_SERIAL_CYCLES;
+    } while (_signal--);
+}
+
+String tinyNextion::get(String data) {
+  switch (print("get " + data)) {
+
+    case NEXTION_CMD_STRING_DATA_ENCLOSED:
+      return _data.substring(0, _data.length() - 3);
+
+    case NEXTION_CMD_NUMERIC_DATA_ENCLOSED:
+      return String((uint32_t(_buffer[4]) << 24) + (uint32_t(_buffer[3]) << 16) + (uint32_t(_buffer[2]) << 8) + uint8_t(_buffer[1]));
+  }
+  return String(_buffer[0]);
+}
+
+int16_t tinyNextion::listen() {
+  if (_serial->available() > 3)
+    if (read()) {
+      uint16_t data = int16_t(_buffer[0]);
+      switch (data) {
+
+        case NEXTION_CMD_STARTUP:
+          break;
+
+        case NEXTION_CMD_TOUCH_COORDINATE_AWAKE:
+        case NEXTION_CMD_TOUCH_COORDINATE_SLEEP:
+          if (_onTouch) _onTouch((uint16_t(_buffer[1]) << 8) | uint8_t(_buffer[2]), (uint16_t(_buffer[3]) << 8) | uint8_t(_buffer[4]), _buffer[5]);
+          break;
+
+        case NEXTION_CMD_TOUCH_EVENT: {
+          if (_onEvent) _onEvent(_buffer[1], _buffer[2], _buffer[3]);
+            nextionCallback *item = _callbacks;
+            while (item) {
+              if ((item->event.page == _buffer[1]) && (item->event.id == _buffer[2]) && (item->event.state == _buffer[3]) && (item->pointer)) {
+                item->pointer(_buffer[1], _buffer[2], _buffer[3]);
+                break;
+              }
+              item = item->next;
+            }
+          }
+          break;
+
+        case NEXTION_CMD_AUTO_ENTER_SLEEP:
+        case NEXTION_CMD_AUTO_ENTER_WAKEUP:
+          break;
+
+        case NEXTION_CMD_READY:
+          break;
+
+        case NEXTION_CMD_START_MICROSD_UPDATE:
+          break;
+      }
+      return data;
+    }
+  return -1;
+}
+
+void tinyNextion::onEvent(nextionOnEvent pointer) {
+  _onEvent = pointer;
+}
+
+void tinyNextion::onTouch(nextionOnTouch pointer) {
+  _onTouch = pointer;
+}
+
+int16_t tinyNextion::page() {
+  if (print("sendme") == NEXTION_CMD_CURRENT_PAGE) return uint8_t(_buffer[1]);
+  else return -1;
+}
+
+uint8_t tinyNextion::print(String data) {
+  flush();
+  send(data);
+  if (read()) return _buffer[0];
+}
+
+uint8_t tinyNextion::read() {
+  uint8_t exit = 0;
+  restore();
+  do if (_serial->available()) {
+      uint8_t data = uint8_t(_serial->read());
+      _signal = NEXTION_SERIAL_CYCLES;
+
+      switch (_buffer[0]) {
+
+        case NEXTION_CMD_STRING_DATA_ENCLOSED:
+          _data += char(data);
+          _length = 1;
+          exit = (data == 0xFF) * (exit + 1);
+          break;
+
+        case NEXTION_CMD_NUMERIC_DATA_ENCLOSED:
+          _buffer[_length++] = uint8_t(data);
+          exit = (_length == 8) * 3;
+          break;
+
+        default:
+          _buffer[_length++] = uint8_t(data);
+          exit = (data == 0xFF) * (exit + 1);
+      }
+    } while (_signal-- && (exit != 3));
+  return _length;
+}
+
+uint8_t tinyNextion::readln() {
+  restore();
+  do while (_serial->available()) {
+      _data += char(_serial->read());
+      _signal = NEXTION_SERIAL_CYCLES;
+    } while (_signal--);
+  return _data.length();
+}
+
+void tinyNextion::restore() {
+  _buffer[0] = 0x00;
+  _data = "";
+  _length = 0;
+  _signal = NEXTION_SERIAL_CYCLES;
+}
+
+void tinyNextion::send(String data) {
+  _serial->print(data + char(0xFF) + char(0xFF) + char(0xFF));
+}
+
+uint32_t tinyNextion::setBaud(uint32_t speed) {
+  flush();
+  send("baud=" + String(speed));
+  return baud();
+}
---
+++ tinyNextion.h
@@ -0,0 +1,85 @@
+#ifndef TINY_NEXTION_H
+#define TINY_NEXTION_H
+
+#include <sys/types.h>
+#include <SoftwareSerial.h>
+
+#define NEXTION_BUFFER_SIZE                10
+#define NEXTION_SERIAL_CYCLES              255
+
+#define NEXTION_EVENT_RELEASE              0
+#define NEXTION_EVENT_PRESS                1
+
+#define NEXTION_CMD_STARTUP                0x00
+#define NEXTION_CMD_TOUCH_EVENT            0x65
+#define NEXTION_CMD_CURRENT_PAGE           0x66
+#define NEXTION_CMD_TOUCH_COORDINATE_AWAKE 0x67
+#define NEXTION_CMD_TOUCH_COORDINATE_SLEEP 0x68
+#define NEXTION_CMD_STRING_DATA_ENCLOSED   0x70
+#define NEXTION_CMD_NUMERIC_DATA_ENCLOSED  0x71
+#define NEXTION_CMD_AUTO_ENTER_SLEEP       0x86
+#define NEXTION_CMD_AUTO_ENTER_WAKEUP      0x87
+#define NEXTION_CMD_READY                  0x88
+#define NEXTION_CMD_START_MICROSD_UPDATE   0x89
+#define NEXTION_CMD_TRANSPARENT_DATA_END   0xFD
+#define NEXTION_CMD_TRANSPARENT_DATA_READY 0xFE
+
+struct nextionComponent {
+  int8_t page, id;
+};
+
+struct nextionEvent {
+  int8_t page, id;
+  bool state;
+};
+
+typedef void (*nextionOnChange) (bool);
+typedef void (*nextionOnEvent) (uint8_t, uint8_t, bool);
+typedef void (*nextionOnPointer) ();
+typedef void (*nextionOnTouch) (uint16_t, uint16_t, bool);
+
+class tinyNextion {
+  protected:
+    uint8_t *_buffer = (uint8_t *) malloc(NEXTION_BUFFER_SIZE);
+    String _data = "";
+    size_t _index = 0;
+    size_t _length = 0;
+    size_t _map;
+    SoftwareSerial *_serial;
+    uint8_t _signal = NEXTION_SERIAL_CYCLES;
+
+  private:
+    struct nextionCallback {
+      nextionCallback *next;
+      nextionEvent event;
+      nextionOnEvent pointer;
+    };
+
+    nextionCallback *_callbacks;
+    nextionOnTouch _onTouch;
+    nextionOnEvent _onEvent;
+
+    uint32_t baud();
+    nextionCallback *callback(nextionEvent event, nextionOnEvent pointer);
+    bool connect();
+    void flush();
+    uint8_t read();
+    uint8_t readln();
+    void restore();
+    void send(String data);
+
+  public:
+    tinyNextion(uint8_t rx, uint8_t tx);
+    uint32_t begin(uint32_t speed = 0);
+    void attach(nextionEvent event, nextionOnEvent pointer);
+    uint8_t backlight(uint8_t value);
+    String get(String data);
+    int16_t listen();
+    void onEvent(nextionOnEvent pointer);
+    void onTouch(nextionOnTouch pointer);
+    int16_t page();
+    uint8_t print(String data);
+    uint32_t setBaud(uint32_t speed);
+};
+
+#endif
